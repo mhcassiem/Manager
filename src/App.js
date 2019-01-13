675f708d3010d321f13c0eb26f2e15a2e34164b8
mhcassiem <mhcassiem@gmail.com>
2019-01-13 12:39:37 +0200

    removed sensitive info

--- src/App.js
+++ src/App.js
@@ -12,12 +12,7 @@ class App extends Component {
   
 	componentDidMount() {
 		firebase.initializeApp({
-			apiKey: 'AIzaSyB04RkXXMv2aRlCVYRTx0CaFSlPOttiL-A',
-			authDomain: 'manager-6ad55.firebaseapp.com',
-			databaseURL: 'https://manager-6ad55.firebaseio.com',
-			projectId: 'manager-6ad55',
-			storageBucket: 'manager-6ad55.appspot.com',
-			messagingSenderId: '182830996762'
+			
 		});
 	}
 
